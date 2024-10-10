043d2bc09fd84126a9041609335c9663f11409d0
twinkal0201 <tinkalpedhadiya556@gmail.com>
2024-10-10 20:14:17 +0530

    Add files via upload

---
+++ src/Layout.js
@@ -0,0 +1,30 @@
+import {Link, Outlet} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+function Layout(){
+    const navigate = useNavigate()
+
+    return(
+
+        <div className="container"> 
+            <div className="row" >
+                <div className="col-2 border border-black p"><h1>Logo</h1></div>
+                <div className='col border border-secondary ' ><Link to="/transactions">Allsummery</Link> </div>
+                <div className='col border border-secondary ' ><Link to="/transactions/add">AddNewEntery</Link> </div>
+                <div className='col border border-secondary '><button className='btn btn-info ' 
+                onClick={() => navigate("/transactions/profit-loss")}>
+                     View Profit/Loss
+                </button></div> 
+            </div>
+            <div className='row'>
+                <div className='col-3 border border-primary'>sidebar</div>
+                <div className='col border border-primary'><Outlet/></div>
+                
+            </div>
+            <div className='row'>
+                <div className='col border border-primary'>footer</div>               
+            </div>
+        </div>
+    )
+}
+export default Layout;
